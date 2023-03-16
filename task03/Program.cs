447ecae7a5a116b44959d95a6e988e0255b3e418
Andranik <andran1401@gmail.com>
2023-03-16 17:11:18 +0400

    task03

---
+++ task03/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("ввидите число");
+string number = Console.ReadLine()!;
+
+void Checkingnumber(string number){
+
+  if (number[0]==number[4] || number[1]==number[3]){
+    
+    Console.WriteLine($"красва это {number} палиндром ");
+    }
+    else Console.WriteLine($"это {number} не палиндром");
+}
+if(number!.Length==5){
+    Checkingnumber(number);
+}
+else Console.WriteLine("ввидите правильное число");
+
